61a69b0c4ed0d21101474f3a4ce509d2495dc1f2
Rajashri Jadhav <Savi@Savi.local>
2020-01-30 14:19:38 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $('.tour-slider').slick({
+        // accessibility: false,
+       
+        //appendArrows: '.header',
+        prevArrow: '.button-prev',
+        nextArrow: '.button-next',
+        infinite: false,
+
+       
+    });
+
+    // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  jQuery ('.menu-btn').on ('click',function(){
+    jQuery ('.header-navbar .navigation').slideToggle(); 
+});
+
+  $( '.tabs' ).rtTab( );
+    
+  });
+
+  
